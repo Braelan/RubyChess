c888d259a3bc86cbbd70b315712446a67d90acd7
Braelan && Cory <Braelan+Cory@appacademy.io>
2015-08-05 12:31:43 -0400

    renders set board w/ color

--- board.rb
+++ board.rb
@@ -2,12 +2,16 @@
 # require_relative 'grid'
 require_relative 'chess_pieces'
 require_relative 'grid'
+require 'colorize'
+
 
 class ChessBoard
    include GRID
 
   BOARD_SIZE = 8
   PIECES = [Rook,Knight, Bishop, King, Queen, Bishop, Knight, Rook]
+  SYMBOLS = {Rook => 'R', Knight => 'H', Bishop => 'B',
+    King => 'K', Queen => 'Q', Pawn => 'P', NilClass => ' '}
 
 def initialize
   new_grid(BOARD_SIZE)
@@ -18,10 +22,10 @@ def on_board?(pos)
 end
 
 def set_board
-  set_pawns(6, :black)
-  set_pawns(1, :white)
-  set_pieces(0, :white)
-  set_pieces(7, :black)
+  set_pawns(1, :blue)
+  set_pawns(6, :red)
+  set_pieces(0, :blue)
+  set_pieces(7, :red)
 end
 
 def set_pawns(row, color)
@@ -39,8 +43,22 @@ def set_pieces(row, color)
 end
 
 def render
-
+  puts "_"*33
+  grid.each do |row|
+    row.each do |cell|
+      symbol = render_piece(cell)
+      print "| #{symbol} "
+    end
+    print "|\n"
+    puts "_"*33
+  end
 end
+
+  def render_piece(cell)
+      symbol = SYMBOLS[cell.class]
+      return symbol.colorize(cell.color) unless cell.nil?
+      symbol
+  end
 
 
 end
