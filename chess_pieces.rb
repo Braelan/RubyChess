6c6ec3b3aba41b262992e077f1dcfeb05780f190
Cory D. Hall <corydantehall@gmail.com>
2015-08-04 22:13:46 -0400

    Created 'chess_pieces.rb' to load all of the necessary 'Chess Piece' classes.

---
+++ chess_pieces.rb
@@ -0,0 +1,6 @@
+require_relative 'chess_pieces/bishop.rb'
+require_relative 'chess_pieces/king.rb'
+require_relative 'chess_pieces/knight.rb'
+require_relative 'chess_pieces/pawn.rb'
+require_relative 'chess_pieces/queen.rb'
+require_relative 'chess_pieces/rook.rb'
